f3447717d549805c13744c95a94f89f4304e4c41
Dotan Nahum <jondotan@gmail.com>
2017-06-15 08:47:55 +0300

    .

--- src/dom/index.js
+++
@@ -1,7 +0,0 @@
-import flight from '../core/flight'
-import { Oval, Rect } from './ui'
-import Controls from './controls'
-const Flight = flight(Rect, Controls)
-
-export default Flight
-export { Oval, Rect }
